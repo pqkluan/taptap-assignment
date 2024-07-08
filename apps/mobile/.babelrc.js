4f7376cf9b8c744bc4610abe6bdaa71921b67ea2
Luan Pham <pqkluan@gmail.com>
2024-07-08 10:30:05 +0700

    build: tweak the initial code style

--- apps/mobile/.babelrc.js
+++ apps/mobile/.babelrc.js
@@ -1,25 +1,22 @@
 module.exports = function (api) {
-  api.cache(true);
+	api.cache(true);
 
-  if (
-    process.env.NX_TASK_TARGET_TARGET === 'build' ||
-    process.env.NX_TASK_TARGET_TARGET.includes('storybook')
-  ) {
-    return {
-      presets: [
-        [
-          '@nx/react/babel',
-          {
-            runtime: 'automatic',
-          },
-        ],
-      ],
-    };
-  }
+	if (
+		process.env.NX_TASK_TARGET_TARGET === 'build'
+	) {
+		return {
+			presets: [
+				[
+					'@nx/react/babel',
+					{
+						runtime: 'automatic',
+					},
+				],
+			],
+		};
+	}
 
-  return {
-    presets: [
-      ['module:@react-native/babel-preset', { useTransformReactJSX: true }],
-    ],
-  };
+	return {
+		presets: [['module:@react-native/babel-preset', { useTransformReactJSX: true }]],
+	};
 };
